44daecc87bc99066bf4fc1adade649b12d3148e4
Chi Zhang <andrea@1x-193.157.224.25.uio.no>
2019-01-11 15:27:07 +0100

    R with database

---
+++ connect.R
@@ -0,0 +1,32 @@
+# first attempt to connect to db
+install.packages('RPostgreSQL')
+library(RPostgreSQL)
+
+# ================ 1. connection
+
+# create a connection
+# save the password that we can "hide" it as best as we can by collapsing it
+pw <- {
+  "andrea"
+}
+
+
+drv <- dbDriver("PostgreSQL")      # loads the PostgreSQL driver
+# creates a connection to the postgres database
+# note that "con" will be used later in each connection to the database
+con <- dbConnect(drv, dbname = "demo",
+                 host = "localhost", port = 5432,
+                 user = "chizhang", password = pw)
+rm(pw) # removes the password
+
+# check tables 
+dbExistsTable(con, "admissions")  # this works
+# TRUE
+
+
+# ================ 2. 
+
+
+
+
+
