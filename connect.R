2bd4564e80b95c9908d889750d30e0aa373c16f9
Chi Zhang <andrea@1x-193.157.224.25.uio.no>
2019-01-12 17:39:24 +0100

    try connection

--- connect.R
+++ connect.R
@@ -1,3 +1,5 @@
+install.packages('tidyverse')
+
 # first attempt to connect to db
 install.packages('RPostgreSQL')
 library(RPostgreSQL)
@@ -14,9 +16,12 @@ pw <- {
 drv <- dbDriver("PostgreSQL")      # loads the PostgreSQL driver
 # creates a connection to the postgres database
 # note that "con" will be used later in each connection to the database
-con <- dbConnect(drv, dbname = "demo",
-                 host = "localhost", port = 5432,
-                 user = "chizhang", password = pw)
+con <- dbConnect(drv, 
+                 dbname = "demo",
+                 host = "localhost", 
+                 port = 5432,
+                 user = "chizhang", 
+                 password = pw)
 rm(pw) # removes the password
 
 # check tables 
@@ -24,8 +29,21 @@ dbExistsTable(con, "admissions")  # this works
 # TRUE
 
 
-# ================ 2. 
+# ================ 2. load data 
+que <- 'SELECT subject_id FROM admissions'  # this could also work
 
+dfsports <- dbGetQuery(con, 
+                       que)
+
+class(dfsports) # it's a dataframe
+
+
+
+
+
+# ================ 3. close connection
+dbDisconnect(con)
+dbUnloadDriver(drv)
 
 
 
