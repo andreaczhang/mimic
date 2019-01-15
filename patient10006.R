9dafb4323a2e582ae89465e9912ce323e7dab612
andrea <andreachizhang@yahoo.com>
2019-01-15 15:10:22 +0100

    query for time events

--- patient10006.R
+++ patient10006.R
@@ -40,4 +40,75 @@ q4.res <- dbGetQuery(con, q4); q4.res
 
 
 
+# --------------- with time stamp: *events 
+# 1. chart events 
+# note that not everything can be numerically visualised. 
+
+q5 <- "
+SELECT subject_id, events.itemid, charttime, label, value, valuenum, valueuom
+FROM chartevents AS events
+INNER JOIN d_items AS itemscode
+ON events.itemid = itemscode.itemid
+WHERE subject_id = 10006
+ORDER BY events.itemid, charttime;
+"
+q5.res <- dbGetQuery(con, q5); q5.res
+head(q5.res)
+dim(q5.res)
+
+
+
+
+# 2. input events 
+
+q6 <- "
+SELECT subject_id, input.itemid, label, amount, amountuom, rate, rateuom, charttime
+FROM inputevents_cv AS input
+INNER JOIN d_items AS itemscode
+ON input.itemid = itemscode.itemid
+WHERE subject_id = 10006
+ORDER BY input.itemid;
+"
+q6.res <- dbGetQuery(con, q6); q6.res
+head(q6.res)
+
+
+
+
+
+# 3. output events 
+q7 <- "
+SELECT subject_id, output.itemid, label, value, valueuom, charttime
+FROM outputevents AS output
+INNER JOIN d_items AS itemscode
+ON output.itemid = itemscode.itemid
+WHERE subject_id = 10006
+ORDER BY output.itemid;
+"
+q7.res <- dbGetQuery(con, q7); q7.res
+head(q7.res)
+
+# 4. lab events 
+q8 <- "
+SELECT subject_id, lab.itemid, label, charttime, value, valuenum, valueuom, flag
+FROM labevents AS lab
+INNER JOIN d_labitems AS labitemscode
+ON lab.itemid = labitemscode.itemid
+WHERE subject_id = 10006
+ORDER BY lab.itemid, charttime;
+"
+q8.res <- dbGetQuery(con, q8); q8.res
+head(q8.res)
+
+# 5. microbiology events 
+
+q9 <- "
+SELECT *
+FROM microbiologyevents
+WHERE subject_id = 10006
+ORDER BY spec_itemid, charttime;
+"
+q9.res <- dbGetQuery(con, q9); q9.res
+head(q9.res)
+
 
